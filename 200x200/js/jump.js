9278e0f244709d3fe78f76ea894fcab288a57561
BoskoB7 <73939828+BoskoB7@users.noreply.github.com>
2021-02-24 01:45:05 +0100

    Add files via upload

--- 200x200/js/jump.js
+++ 200x200/js/jump.js
@@ -6,39 +6,20 @@ function showIt() {
 
   // !!! when all that is commented out is erased the first part functions (why it matters?)!!!
 
-//   var w = window.innerWidth/2;
-//   var h = window.innerHeight/2;
-  // function center() {
 
 
-//   var element = document.getElementById("div1");
-//   element.style.left = x + "px";
-//   element.style.top = y + "px";
-// }
+  function center() {
 
-// document.getElementById("div1").style.left = w + "px";
-// document.getElementById("div1").style.top = h + "px";
+    var w = window.innerWidth/2-100;
+    var h = window.innerHeight/2-100;
+
+  var element = document.getElementById("div1");
+  element.style.left = w + "px";
+  element.style.top = h + "px";
+
+
 }
-// window.onload = () => {
-//     const dog = document.getElementById("div1");
-//     dog.addEventListener("mouseover", () => {
-//       dog.style.width = "500px";
-//     });
-//     dog.addEventListener("mouseout", () => {
-//       dog.style.width = "200px";
-//     });
-//   };
-
-//   window.onclick = () => {
-//     const dog = document.getElementById("div1");
-//     dog.addEventListener("onclick", () => {
-//       dog.style.top = "w";
-//       dog.style.left = "h";
-//     });
-//   };
-
-// var position []
-// position[0].style.position = "absolute";
-// position[0].style.left = w + 'px';
-// position[0].style.top = h + 'px';
-// document.body.appendChild(position[0]);
+window.addEventListener("resize",function(){
+  center();
+})
+// first event what are you ListeningStateChangedEvent, mouse OverconstrainedError, mouseout, down, and the secon event is a function
